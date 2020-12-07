3c3e4a6af7c94c2085a882eeaa5c47996aa8d852
Vcthtuf <vcthtuf@gmail.com>
2020-12-07 22:53:37 +0500

    Add effect

--- build/js/script.js
+++ build/js/script.js
@@ -13,19 +13,21 @@ window.addEventListener('DOMContentLoaded', () => {
             n = 0;
         }
 
+        if (n < 0) { n = headerSlides.length - 1; }
+
         for (let i = 0; i < headerSlides.length; i++) {
             //headerSlides[i].display = "none";
             headerSlides[i].style.width = "0%";
-            headerSlides[i].style.height = "0%";
+            // headerSlides[i].style.height = "0%";
             dots[i].classList.remove('header_dot_active');
 
         }
         //headerSlides[n].display = "block";
         headerSlides[n].style.width = "100%";
-        headerSlides[n].style.height = "100%";
+        // headerSlides[n].style.height = "100%";
         dots[n].classList.add('header_dot_active');
     };
 
-    setInterval(function () { showSlides(i); i++; if (i > headerSlides.length) { i = 1; } console.log(i); }, 2000);
+    setInterval(function () { showSlides(i); i++; if (i > headerSlides.length) { i = 1; } console.log(i); }, 4000);
 
 });
