f608d6e8a0a38db1dd45d02a2614f77fe1190e80
Vcthtuf <vcthtuf@gmail.com>
2020-12-13 00:47:59 +0500

    Added mobile menu and event click

--- build/js/script.js
+++ build/js/script.js
@@ -42,9 +42,11 @@ window.addEventListener('DOMContentLoaded', () => {
 
     // hamburger 
 
-    let hamburger = document.querySelector('.hamburger');
+    let hamburger = document.querySelector('.hamburger'),
+        mobileMenu = document.querySelector('.mobile_menu');
     hamburger.addEventListener('click', function () {
         hamburger.classList.toggle('hamburger_active');
+        mobileMenu.classList.toggle('mobile_menu_active');
     });
 
 });
